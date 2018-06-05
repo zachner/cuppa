eebc331fc49e1d38540d78e21d4bb1aa8e141aa8
Raghu Betina <raghu@firstdraft.com>
2018-06-05 18:00:14 +0000

    Whitelist columns in BeanLike for ActiveAdmin

--- app/admin/bean_likes.rb
+++ app/admin/bean_likes.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register BeanLike do
+
+ permit_params :user_id, :bean_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
