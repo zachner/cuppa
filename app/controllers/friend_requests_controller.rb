826dbc91f697a0c1a663a6e93523744da2f194ba
Raghu Betina <raghu@firstdraft.com>
2018-06-05 18:00:21 +0000

    Only friend_request user can modify friend_request

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,4 +1,14 @@
 class FriendRequestsController < ApplicationController
+  before_action :current_user_must_be_friend_request_sender, :only => [:edit, :update, :destroy]
+
+  def current_user_must_be_friend_request_sender
+    friend_request = FriendRequest.find(params[:id])
+
+    unless current_user == friend_request.sender
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @friend_requests = FriendRequest.all
 
