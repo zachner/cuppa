5e864bbd5dac56cda193203c324e8eea26a5e847
Raghu Betina <raghu@firstdraft.com>
2018-06-05 18:00:17 +0000

    Whitelist columns in RoasterFollow for ActiveAdmin

--- app/admin/roaster_follows.rb
+++ app/admin/roaster_follows.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register RoasterFollow do
+
+ permit_params :user_id, :roaster_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
