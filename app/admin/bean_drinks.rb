13c5ef6c5cace5e4a5f77321f6f3dbeec65f5df6
Raghu Betina <raghu@firstdraft.com>
2018-06-05 18:00:06 +0000

    Whitelist columns in BeanDrink for ActiveAdmin

--- app/admin/bean_drinks.rb
+++ app/admin/bean_drinks.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register BeanDrink do
+
+ permit_params :bean_id, :drink_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
