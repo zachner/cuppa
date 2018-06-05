3bd782762ebcb7a509e01207037f7883f2e8fad7
Raghu Betina <raghu@firstdraft.com>
2018-06-05 17:59:09 +0000

    Validate uniqueness of users.username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :username, :uniqueness => true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
