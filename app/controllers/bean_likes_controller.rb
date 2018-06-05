9dde34b3e70b28f33c8c3430469439134ca9810d
Raghu Betina <raghu@firstdraft.com>
2018-06-05 18:00:21 +0000

    Only bean_like user can access show page

--- app/controllers/bean_likes_controller.rb
+++ app/controllers/bean_likes_controller.rb
@@ -1,5 +1,5 @@
 class BeanLikesController < ApplicationController
-  before_action :current_user_must_be_bean_like_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_bean_like_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_bean_like_user
     bean_like = BeanLike.find(params[:id])
